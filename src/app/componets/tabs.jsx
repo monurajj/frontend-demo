5fc66d973c7b3e8213dd0f464086a79129feb47f
Monu Rajj <monu2feb2004.com>
2024-06-14 12:38:16 +0530

    demo-project iir

---
+++ src/app/componets/tabs.jsx
@@ -0,0 +1,10 @@
+
+export default function Tabs() {
+  return (
+    <div className="mt-20 flex justify-center items-center mb-20">
+      <button className="text-center font-medium py-2 px-6 rounded-lg border-none text-sm bg-white text-black">Schemes</button>
+      <button className="text-center font-medium py-2 px-6 rounded-lg border-none text-sm bg-white text-black">Job Openings</button>
+      <button className="text-center font-medium py-2 px-6 rounded-lg border-none text-sm bg-white text-black">Scholarships</button>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Tabs from './componets/tabs'
+export default function Home() {
+  return (
+    <div>
+       <Tabs/>
+    </div>
+  );
+}
